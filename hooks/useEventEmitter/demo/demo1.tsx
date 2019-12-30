47cb52ce5b85305cbc75cebc18c7ce7077f85498
吴震 <wuzhen01@corp.netease.com>
2019-12-30 17:04:38 +0800

    update

---
+++ hooks/useEventEmitter/demo/demo1.tsx
@@ -0,0 +1,43 @@
+import React, { useRef, FC } from "react";
+import { Button, Input } from "antd";
+
+import useEventEmitter, { EventEmitter } from "../index";
+
+const MessageBox: FC<{
+  focus$: EventEmitter<void>;
+}> = function(props) {
+  return (
+    <div style={{ paddingBottom: 24 }}>
+      <p>You received a message</p>
+      <Button
+        onClick={() => {
+          props.focus$.emit();
+        }}>
+        Reply
+      </Button>
+    </div>
+  );
+};
+
+const InputBox: FC<{
+  focus$: EventEmitter<void>;
+}> = function(props) {
+  const inputRef = useRef<any>();
+  props.focus$.useSubscription(() => {
+    inputRef.current.focus();
+  });
+  props.focus$.useSubscription(() => {
+    console.log("do some things in this component...");
+  });
+  return <Input ref={inputRef} placeholder="Enter reply" />;
+};
+
+export default function() {
+  const focus$ = useEventEmitter();
+  return (
+    <>
+      <MessageBox focus$={focus$} />
+      <InputBox focus$={focus$} />
+    </>
+  );
+}
